eabc2c021eb2d75e1b5197890dee6d5bf7daee4b
lzch <lzch@localhost.com>
2019-02-28 20:22:44 +0800

    add commit.bat file__2019-02-28 20:22:36.652

---
+++ commit.bat
@@ -0,0 +1,12 @@
+@echo off
+
+for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
+set ldt=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2% %ldt:~8,2%:%ldt:~10,2%:%ldt:~12,6%
+
+set /p message=Input commit message: 
+
+git add .
+
+git commit -m "%message%__%ldt%"
+
+git push -u origin master
